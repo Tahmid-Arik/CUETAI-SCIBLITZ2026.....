4907f3312aa7ec95da53d6c61ad5d0267af8e90d
Tahmid-Arik <tahmidbinismail285@gmail.com>
2026-06-27 05:19:08 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -17,21 +17,21 @@ else:
     st.error("API Key not found! Please set GEMINI_API_KEY in your .env file.")
 
 
-st.title("My Streamlit App-SciBlitz 2026")
-st.subheader("I am Tahmid Arik. Welcome to my AI application!")
+st.title("Health and Medical Care App-SciBlitz 2026")
+st.subheader("Welcome to my AI application!Do you have Any questions about your Health or you want to know about a disease? Ask me anything and I will try to provide you with the best possible answer based on the information available in my context file.")
 
 
-DATA_FILE_PATH = os.path.join(BASE_DIR, "data", "cuet_info.txt")
+DATA_FILE_PATH = os.path.join(BASE_DIR, "data", "disease_info.txt")
 
 def load_context():
     if os.path.exists(DATA_FILE_PATH):
         with open(DATA_FILE_PATH, "r", encoding="utf-8") as file:
             return file.read()
     else:
-        st.error("Data file missing! Please create 'data/cuet_info.txt'")
+        st.error("Data file missing! Please create 'data/disease_info.txt'")
         return ""
 
-user_input = st.text_input("Ask me anything about CUET!")
+user_input = st.text_input("write your info here")
 
 if st.button("give the answer"):
     if user_input:
@@ -40,8 +40,7 @@ if st.button("give the answer"):
             
             if context_data:
                 prompt = f"""
-                You are a smart university assistant for CUET. 
-                Answer the user's question based strictly on the provided Context below.
+                You are a smart Health and Medical assistant for Humans.If a user gives the symptoms of a disease, you will provide the most likely disease name and its treatment according to the context below. If the user asks about a specific disease, you will provide detailed information about that disease, including its symptoms, causes, and treatment options according to the context below. If the user asks for general health advice, you will provide tips for maintaining good health and preventing illness according to the context below.You can use your own intelligence to provide the user information within the context below but do not go outside the contewxt.
                 Context:
                 {context_data}
 
